9dda91d1073ee2ee91ada8b1cec826921788a775
Evan Combs <evan@combs-family.com>
2022-03-01 15:30:18 -0500

    Implemented queue based lock ShowroomQueue to simulate queue to vase showroom 	ShowroomQueue does NOT implement the lock interface updated README with discussion of implementation

--- MinotaurVase.java
+++ MinotaurVase.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.lang.Runnable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+
 class MinotaurVase
 {
   public static void main(String[] args)
@@ -16,7 +22,7 @@ class MinotaurVase
 
     ArrayList<Thread> guests = new ArrayList<>();
 
-    Showroom showroom = new Showroom();
+    Showroom showroom = new Showroom(numGuests);
     for (int i = 0; i < numGuests; i++)
     {
       Thread guest = new Thread(showroom, String.valueOf(i));
@@ -29,12 +35,43 @@ class MinotaurVase
 
 }
 
-class Showroom extends Runnable
+// Showroom simulates the guests trying to view the vase
+class Showroom implements Runnable
 {
+  ShowroomQueue queueLock;
+
+  public Showroom(int numGuests)
+  {
+     queueLock = new ShowroomQueue(numGuests);
+  }
 
   public void run()
   {
+    // All threads will reach here, and hang while the guests (thread) that has
+    // acquired the lock looks at the vase;
+    enjoyParty();
+    boolean interestedInVase = true;
+    while(interestedInVase)
+    {
+      queueLock.lock();
+      try
+      {
+        lookAtVase();
+      }
+      finally
+      {
+        queueLock.unlock();
+      }
+      // interestedInVase = false;
+      // The vase is so enthralling that a guest may decide to get back in line again.
 
+      if (Math.random() <= 0.5)
+        continue;
+      else
+      {
+        interestedInVase = false;
+      }
+    }
   }
 
 
@@ -42,5 +79,70 @@ class Showroom extends Runnable
   {
     System.out.println("Guest " + Thread.currentThread().getName() +
                        " looks at the vase, and is very impressed.");
+  }
+
+  // Adds a small bit of random time
+  public void enjoyParty()
+  {
+    try
+    {
+      Thread.sleep((long) Math.random());
+    }
+    catch(Exception e)
+    {
+
+    }
+  }
+}
+
+// ShowroomQueue is a circular array-based lock
+// Modeled after ALock from Art of Multiprocessor Programming, pg 150
+class ShowroomQueue
+{
+  int size;
+  AtomicInteger tail;
+  volatile boolean[] flag;
+  ThreadLocal<Integer> threadIndex;
+
+  public ShowroomQueue(int capacity)
+  {
+    // Setup intial lock properties
+    size = capacity;
+    tail = new AtomicInteger(0);
+    flag = new boolean[capacity];
+    flag[0] = true;
+
+    threadIndex = new ThreadLocal<Integer>()
+    {
+      protected Integer initialValue()
+      {
+        return 0;
+      }
+    };
+
+  }
+
+  public void lock()
+  {
+    // Tail always increments, since the queue is circular
+    int slot = tail.getAndIncrement() % size;
+
+    // Store the slot this thread is waiting at
+    threadIndex.set(slot);
+
+    // Hang here until this threads slot becomes unlocked
+    while (!flag[slot])
+      ; // wait
+  }
+
+  public void unlock()
+  {
+    int slot = threadIndex.get();
+
+    // Flag that this slot is now free
+    flag[slot] = false;
+
+    // "Notify" the next thread
+    flag[(slot + 1) % size] = true;
   }
 }
--- README.md
+++ README.md
@@ -18,8 +18,12 @@ at least one guest has been here before. When the leader has found an empty plat
 N times, that means that every guest has visited the maze at least once, since
 an empty plate only arises from a unique guest eating a cupcake.
 
-My solution makes use of two atomic variables, but they are not used to communicate between guests except for the status
-of the cupcake, and the status of the end of the "game".
+The "randomness" of the guest scheduling is simulated by the random nature of thread
+assignment. Each guest (thread) continually enters the maze; it is impossible to know
+in what order they do so.
+
+My solution makes use of two atomic variables, but they are not used to communicate between guests except for the status of the cupcake, and the status of the end of the "game" (so the guests know when to
+stop entering the maze).
 
 
 ## Problem 2: Minotaur’s Crystal Vase
@@ -43,9 +47,27 @@ this approach is NOT starvation-free.
 
 
 Approach 2) This approach is most similar to a blocking approach, where guests
-who see that the showroom is "Busy" are free to enjoy the party instead.
+who see that the showroom is "Busy" are free to enjoy the party instead. This has
+a similar problem to approach 1, however, in that a guest has no guarantee that
+they will get to see the vase, for example if it is always "Busy" when they go
+to check.
+
 
 Approach 3) This approach is the best approach to ensure that all guests who want to
 will eventually get to see the vase, i.e. starvation-free. The downside to this,
 however, is that like with Approach 1, guests waiting for the vase are unable to
 enjoy the party while waiting.
+
+Since the Minotaur wants to show off his vase to everyone who wants to see it,
+and wants to ensure that everyone who wants to is able to, this approach is the
+best to ensure this.
+
+### Implementation
+For my implementation I have implemented a circular array-based lock similar
+to ALock from pg 150 of the textbook. Threads (guests) maintain a slot in a queue and
+use a volatile boolean array to track which threads acquire the lock.
+
+The situation is simulated in the Showroom class, where guests can either look at the
+vase (and be impressed) or spend a random amount of time at the party. All guests
+will want to look at the vase at least once, and thus will get in line; the queue
+lock ensures that all guests who queue will eventually get to see the vase (starvation-freedom).
